ef73fa534a3ed00953b8d25c37272ca4fc496898
malikspruill <malikspruill@gmail.com>
2022-08-08 16:59:23 -0400

    Add additions to prepare database with sqlite

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+using Domain; //import from Domain sub-directory
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Activity> ? Activities { get; set; }
+    }
+}
